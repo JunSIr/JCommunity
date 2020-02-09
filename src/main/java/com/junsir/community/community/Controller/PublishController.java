fbec658e0fe2b94d14ca6658d2dc8691f388265c
JunSIr <56471335+JunSIr@users.noreply.github.com>
2020-02-10 01:10:02 +0800

    add publish.html and its controller

---
+++ src/main/java/com/junsir/community/community/Controller/PublishController.java
@@ -0,0 +1,16 @@
+package com.junsir.community.community.Controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+    public class PublishController {
+
+
+        @GetMapping("/publish")
+        public String toPublish() {
+
+            return "publish";
+        }
+
+    }
+
