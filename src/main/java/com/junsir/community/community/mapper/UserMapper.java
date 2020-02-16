a722c6014f2fd169c90c79e9200902732aa37e9d
JunSIr <56471335+JunSIr@users.noreply.github.com>
2020-02-16 08:27:56 +0800

    Deployed to my TecentYun

--- src/main/java/com/junsir/community/community/mapper/UserMapper.java
+++ src/main/java/com/junsir/community/community/mapper/UserMapper.java
@@ -6,7 +6,7 @@ import org.apache.ibatis.annotations.*;
 @Mapper
 public interface UserMapper {
 
-    @Select("SELECT * FROM GITHUB_USER WHERE ID = #{id}")
+    @Select("SELECT * FROM github_user WHERE ID = #{id}")
     @Results({
             @Result( property = "login",column = "user_name"),
             @Result( property = "avatarUrl",column = "avatar_url")
@@ -14,7 +14,7 @@ public interface UserMapper {
 
 
 
-    @Select("SELECT * FROM GITHUB_USER WHERE token = #{token}")
+    @Select("SELECT * FROM github_user WHERE token = #{token}")
     @Results({
             @Result( property = "login",column = "user_name"),
             @Result( property = "avatarUrl",column = "avatar_url")
@@ -24,7 +24,7 @@ public interface UserMapper {
     /*注解Param内的参数与数据库表对应
     * userName则无关
     * */
-   /* @Insert("insert into GITHUB_USER (user_name, id, url) values (#{user_name},#{id},#{url})")
+   /* @Insert("insert into github_user (user_name, id, url) values (#{user_name},#{id},#{url})")
     int insertInToMySql(@Param("id") String id,@Param("user_name") String userName,@Param("url") String url);*/
 
 
@@ -33,21 +33,21 @@ public interface UserMapper {
 * 原因：与批量插入操作混淆
 * 如配置情况下：collection="personList" separator="," item="person" ->此种情况下应该使用person.形式
 * */
-/*    @Insert("insert into GITHUB_USER (user_name, id, url) values (#{user.login},#{user.id},#{user.url})")
+/*    @Insert("insert into github_user (user_name, id, url) values (#{user.login},#{user.id},#{user.url})")
     @Results({
             @Result( property = "login",column = "user_name")
     })
     int insertInToMySql(GitHubUser user);*/
 
 
-    @Insert("insert into GITHUB_USER (user_name, id, url,token, avatar_url) values (#{login},#{id},#{url},#{token},#{avatarUrl})")
+    @Insert("insert into github_user (user_name, id, url,token, avatar_url) values (#{login},#{id},#{url},#{token},#{avatarUrl})")
     @Results({
             @Result( property = "login",column = "user_name"),
             @Result( property = "avatarUrl",column = "avatar_url")
     })
     int insertInToMySql(GitHubUser user);
 
-    @Insert("insert into GITHUB_USER (user_name, id, url,token, avatar_url) values (#{login},#{id},#{url},#{token},#{avatarUrl})")
+    @Insert("insert into github_user (user_name, id, url,token, avatar_url) values (#{login},#{id},#{url},#{token},#{avatarUrl})")
     @Results({
             @Result( property = "login",column = "user_name"),
             @Result( property = "avatarUrl",column = "avatar_url")
